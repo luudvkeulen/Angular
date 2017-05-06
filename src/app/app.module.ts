07b096419429a434e3d96539a8a1e35829002bfd
Luud van Keulen <l.vankeulen@student.fontys.nl>
2017-05-06 19:01:36 +0200

    More nav links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import {RouterModule} from "@angular/router";
 
 import {AppComponent} from './app.component';
 import {headerComponent} from './header/app.headerComponent';
-import {contentAreaComponent} from "./contentArea/app.contentAreaComponent";
 import {informationComponent} from "./information/app.informationComponent";
 import {placesComponent} from "./places/app.placesComponent";
 
@@ -16,7 +15,6 @@ import {placesComponent} from "./places/app.placesComponent";
   declarations: [
     AppComponent,
     headerComponent,
-    contentAreaComponent,
     informationComponent,
     placesComponent
   ],
--- src/app/contentArea/app.contentAreaComponent.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  templateUrl: './app.contentAreaComponent.html'
-})
-
-export class contentAreaComponent {
-
-}
