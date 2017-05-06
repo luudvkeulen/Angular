575a9d559cc56616b42847d57617aebcfe06fecf
Luud van Keulen <l.vankeulen@student.fontys.nl>
2017-05-07 00:08:41 +0200

    Nav fix. Card information

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
-import {MaterializeModule} from "angular2-materialize";
 import {RouterModule, Routes} from "@angular/router";
 
 import {AppComponent} from './app.component';
@@ -38,7 +37,6 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterializeModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
