b0fbf0d274ede25826a9e5ade1283c8a013ab013
Luud van Keulen <l.vankeulen@student.fontys.nl>
2017-05-08 12:04:37 +0200

    Dist only for github pages

--- src/app/information/information.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  templateUrl: 'information.component.html'
-})
-
-export class InformationComponent {
-
-}
--- src/app/places/places.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  templateUrl: 'places.component.html'
-})
-
-export class PlacesComponent {
-
-}
