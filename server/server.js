2124265a0e1da9371b8bb70c4aff7475a3d18abf
Luud van Keulen <l.vankeulen@student.fontys.nl>
2017-05-08 15:25:03 +0200

    Node.js hosting and fixed some references

--- server/server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var path = require('path');
-
-var app = express();
-
-var port = process.env.PORT || 8080;
-app.listen(port, function () {
-  console.log('Server running at port ' + port);
-});
