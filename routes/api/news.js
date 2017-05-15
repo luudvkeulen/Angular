62b1fd99894eff6e2d361e2568141f365e2c7f62
Luud van Keulen <l.vankeulen@student.fontys.nl>
2017-05-15 10:53:22 +0200

    Limit amount of request size

--- routes/api/news.js
+++ routes/api/news.js
@@ -1,5 +1,7 @@
 const articlesFile = require('../../articles.json');
 
 module.exports = function(req, res) {
-  res.status(200).json(articlesFile.slice(req.query.begin,req.query.end));
+  var begin = (req.query.begin >= 0) ? req.query.begin : 0;
+  var end = ((req.query.end - req.query.begin) <= 10) ? req.query.end : 10;
+  res.status(200).json(articlesFile.slice(begin,end));
 };
