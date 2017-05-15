061c5bd73af56418f46163f70b14778ad9ff25c1
Luud van Keulen <l.vankeulen@student.fontys.nl>
2017-05-15 14:13:48 +0200

    Removed component css

--- src/app/create-news/create-news.component.ts
+++ src/app/create-news/create-news.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-create-news',
-  templateUrl: './create-news.component.html',
-  styleUrls: ['./create-news.component.css']
+  templateUrl: './create-news.component.html'
 })
 export class CreateNewsComponent implements OnInit {
 
